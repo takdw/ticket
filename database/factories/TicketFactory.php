17c7301d5da2f31614c3b74f05b4a004fb9e825d
Tamrat Assefa Kessito <mamushassefa@gmail.com>
2020-11-19 13:42:44 +0300

    fix failing tests by providing factory defaults

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Ticket;
 use App\Models\Vendor;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
 
 class TicketFactory extends Factory
 {
@@ -23,10 +24,16 @@ class TicketFactory extends Factory
     public function definition()
     {
         return [
+            'title' => $this->faker->sentence,
+            'subtitle' => $this->faker->sentence,
+            'date' => Carbon::parse('20-10-2020 12:00PM'),
+            'venue' => $this->faker->word,
+            'city' => $this->faker->city,
             'price' => 2500,
             'vendor_id' => function() {
                 return Vendor::factory()->create()->id;
-            }
+            },
+            'additional_info' => $this->faker->sentence,
         ];
     }
 }
