64ad45429109b44498ed402fa5de2b46ee94062a
Tamrat Assefa Kessito <mamushassefa@gmail.com>
2020-11-22 03:24:26 +0300

    setup a database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\Ticket;
+use App\Models\User;
+use App\Models\Vendor;
+use App\Models\Wallet;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +18,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Create an Admin Role
+        $adminRole = Role::factory()->create(['name' => 'admin']);
+
+        // Create an Admin
+        $admin = User::factory()->create(['email' => 'admin@admin.com', 'name' => 'Admin']);
+        $admin->roles()->sync($adminRole->id);
+
+        // Create a User
+        $user = User::factory()->create(['email' => 'mamushassefa@gmail.com']);
+
+        // Add balance to the user's wallet
+        Wallet::where('user_id', $user->id)->first()->update(['amount' => 200000]);
+
+        // Create a Vendor
+        $vendor = Vendor::factory()->create(['tin' => '0022334455', 'name' => 'Sheger Events and Promotions']);
+
+        // Create a Ticket for the Vendor
+        Ticket::factory()->create(['vendor_id' => $vendor->id]);
     }
 }
